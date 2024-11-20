71835eecefa2a60321f9e9fcbb545d348ae70dce
Muhammad Faisal (Asst. Manager Web App - ICT) <faisal@seltech.com>
2024-11-20 10:13:35 +0500

    nextjs-ecommerce project

--- src/app/about3/page.js
+++ src/app/about3/page.js
@@ -36,7 +36,7 @@ export default function About3() {
           
       <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
         {products.map((pro) => {
-          let prod_url = `${process.env.VERCEL_URL}/about4/${pro.id}`;
+          let prod_url = `${process.env.NEXT_PUBLIC_BASE_URL}/about4/${pro.id}`;
 
           return (
             <div key={pro.id} className="product-card" style={{
